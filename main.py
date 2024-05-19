b908ffe69a7361ce42cc8306a34f6d51f5a900b8
Dev <100162367+pawyercito@users.noreply.github.com>
2024-05-19 13:11:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-# handler.py
+# main.py
 from fastapi import FastAPI
-from controllers.user_register_controller import router as user_router
-from db.database import engine, Base
-from fastapi.middleware.cors import CORSMiddleware
-from controllers.login_controller import router as login_router
-from controllers.update_patient_information_controller import router as update_patient_information_router
+from handlers import app  # Importa la instancia de FastAPI desde handler.py
 
-app = FastAPI()
-
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-app.include_router(user_router)
-app.include_router(login_router)
-app.include_router(update_patient_information_router)
-
-# Configurar CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # Ajusta esto a la URL de tu frontend
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.on_event("startup")
-async def startup():
-    print("Starting up...")
-    Base.metadata.create_all(bind=engine)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
