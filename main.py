48d5677e7e4a7ff55d6026ec473b0a87475150b8
Dev <100162367+pawyercito@users.noreply.github.com>
2024-05-19 14:24:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from fastapi import FastAPI
 import os
-from fastapi import FastAPI
-from controllers.user_register_controller import router as user_router
-from db.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
+from controllers.user_register_controller import router as user_router
 from controllers.login_controller import router as login_router
 from controllers.update_patient_information_controller import router as update_patient_information_router
+from db.database import engine, Base
 
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "IM ALIVE!"}
 
 # Configurar CORS
 app.add_middleware(
@@ -22,6 +17,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Incluir rutas de los controladores
+app.include_router(user_router)
+app.include_router(login_router)
+app.include_router(update_patient_information_router)
+
+@app.get("/")
+def read_root():
+    return {"message": "IM ALIVE!"}
+
 @app.on_event("startup")
 async def startup():
     print("Starting up...")
@@ -31,5 +35,3 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-
-
